c9128f712ecc38506f593d8df1bfbf433ef3a4a5
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2020-05-25 15:11:52 +0200

    problem 005 solved

--- src/005.jl
+++ src/005.jl
@@ -1,10 +1,28 @@
 function e005(N)
- return 0
- end
+    return prod_of_factors(N)
+end
+
+function prod_of_factors(n)
+    factors = zeros(Int8, n+1)
+    for i = 2:n
+        channel = Channel(c->get_prime_factors(c, i))
+        for (f, mul) in channel
+            if factors[f] < mul
+                factors[f] = mul
+            end
+        end
+    end
+    prod = 1
+    for i = 2:n
+        prod *= i^factors[i]
+    end
+    return prod
+end
+
  
- function solution005()
- N = 0
- answer = e005(N)
- println("The answer to problem 005 is $answer")
- end
+function solution005()
+    N = 20
+    answer = e005(N)
+    println("The answer to problem 005 is $answer")
+end
 
--- test/005.jl
+++ test/005.jl
@@ -1,4 +1,4 @@
 @testset "005" begin
- @test tdpe.e005(0) == 1
- end
+    @test tdpe.e005(10) == 2520
+end
 
