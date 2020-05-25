af8f5f198c6c547f33b17beee8f7c69681557678
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2020-05-25 09:31:51 +0200

    Problem 003 solved.

--- src/003.jl
+++ src/003.jl
@@ -1,9 +1,27 @@
 function e003(N)
-    return 0
+    return largest_factor(N)
+end
+
+function largest_factor(N::Integer)
+    if N < 2
+        return 1
+    else
+        f::Integer = 2
+        while f <= sqrt(N)
+            while N % f == 0
+                N ÷= f
+                if N == 1
+                    return f
+                end
+            end
+            f += 1
+        end
+        return N
+    end
 end
  
 function solution003()
-    N = 0
+    N = 600851475143
     answer = e003(N)
     println("The answer to problem 003 is $answer")
 end
--- test/003.jl
+++ test/003.jl
@@ -1,4 +1,6 @@
 @testset "003" begin
-    @test tdpe.e003(0) == 1
+    @test tdpe.e003(4) == 2
+    @test tdpe.e003(9) == 3
+    @test tdpe.e003(13195) == 29
 end
 
