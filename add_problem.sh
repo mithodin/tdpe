21a889db453909de82933848eb17da8dab0ff7a6
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2020-05-28 08:33:15 +0200

    better add_problem script

--- add_problem.sh
+++ add_problem.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 number=$1
 
-srcfile="function e$number(N)\n
-    return 0\n
-end\n
-\n
-function solution$number()\n
-    N = 0\n
-    answer = e$number(N)\n
-    println(\"The answer to problem $number is \$answer\")\n
+srcfile="function e$number(N)
+    return 0
+end
+
+function solution$number()
+    N = 0
+    answer = e$number(N)
+    println(\"The answer to problem $number is \$answer\")
 end\n"
 
-testfile="@testset \"$number\" begin\n
-    @test tdpe.e$number(0) == 1\n
+testfile="@testset \"$number\" begin
+    @test tdpe.e$number(0) == 1
 end\n"
 
-echo -e $srcfile > src/$number.jl
-echo -e $testfile > test/$number.jl
+echo -e "$srcfile" > src/$number.jl
+echo -e "$testfile" > test/$number.jl
 
 sed -i 's/#end prints/solution'$number'()\n    #end prints/g' src/tdpe.jl
 sed -i 's/#end includes/include("'$number'.jl")\n#end includes/g' src/tdpe.jl
